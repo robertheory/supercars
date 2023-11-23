255eb2b4936f6e70444a32407d531e5174ed9f49
Roberto Costa <roberto.araujo339@gmail.com>
2023-11-23 12:26:44 -0300

    Update environment variable usage in database and main files

--- server/app/database.py
+++ server/app/database.py
@@ -1,12 +1,12 @@
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from os import getenv
+from os import environ
+from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = getenv("DATABASE_URL")
+DATABASE_URL = environ["DATABASE_URL"]
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
