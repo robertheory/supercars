871a9eeeac152e5c0ca74e467ec74c561ad85936
Roberto Costa <roberto.araujo339@gmail.com>
2023-11-23 17:28:09 -0300

    Update database connection settings

--- server/app/database.py
+++ server/app/database.py
@@ -8,9 +8,8 @@ load_dotenv()
 
 DATABASE_URL = environ["DATABASE_URL"]
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
